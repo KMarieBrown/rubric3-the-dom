3e7a20811dcd83cc58d71f0f239ea97de4538b11
K. Marie Brown <k.mariebrown@outlook.com>
2020-09-16 17:18:00 -0500

    📦 NEW: Add main JavaScript file.

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+'use script';
+// Test main JS file.
+console.log('Your main JS file is working fine.');
+
+// Change the second heading from a first level heading to a second level heading.
+document.getElementById('beginning').outerHTML = "<h2>It's Only The Beginning!</h2>";
