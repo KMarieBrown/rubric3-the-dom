2a768ffda218902750504040f766552095fa1210
K. Marie Brown <k.mariebrown@outlook.com>
2020-09-18 15:29:26 -0500

    📦 NEW: Add btn function, 2nd h1 & format changes.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,5 +2,27 @@
 // Test main JS file.
 console.log('Your main JS file is working fine.');
 
-// Change the second heading from a first level heading to a second level heading.
+// Changes the second heading from a first level heading to a second level heading.
 document.getElementById('beginning').outerHTML = "<h2>It's Only The Beginning!</h2>";
+
+// Centers the aside element.
+var myList = document.getElementById('toDo');
+myList.setAttribute('align', 'center');
+
+// Console logs the number of list items in the document.
+var myDemoTags = document.getElementsByTagName('li');
+console.log('Number of li links: ' + myDemoTags.length);
+
+// Returns the nmber of items listed in the aside, in red font, when the button is clicked.
+function remember() {
+	var linksInList = myList.getElementsByTagName('li');
+	document.querySelector('#redundant').innerHTML = myDemoTags.innerHTML =
+		'Remember, keep these ' + linksInList.length + ' things in mind!';
+	document.querySelector('#redundant').style.cssText = 'color: red; font-weight: bold; font-size: 22px;';
+}
+
+// Centers all level 1 headings.
+document.querySelector('h1').style.textAlign = 'center';
+
+// Centers all level 2 headings.
+// document.querySelector('h2').style.textAlign = 'center';
